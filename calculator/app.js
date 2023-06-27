e877d1a5ee9fcfebe02a44843b9b59807935e6ea
bolaji <ibrahimkenny17@gmail.com>
2023-06-27 08:11:17 +0100

    calculator project

---
+++ calculator/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  const num1 = Number(req.body.num1);
+  const num2 = Number(req.body.num2);
+  const sum = num1 + num2;
+  res.send(`The sum is ${sum}`);
+});
+
+app.listen(3000, (req, res) => {
+  console.log("Listening on port 3000");
+});
